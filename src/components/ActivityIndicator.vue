9805d17a04718d36536547d0416e724c99c6ed1e
John S <diaz.john312@gmail.com>
2018-12-07 18:47:44 -0500

    activity indicator

---
+++ src/components/ActivityIndicator.vue
@@ -0,0 +1,39 @@
+<template>
+  <img
+    v-bind:src="require('../assets/img/nebula.png')"
+    class="activity-indicator" 
+    v-bind:style="{ height: chosenSize + 'px', width: chosenSize + 'px' }"
+  />
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+
+@Component
+export default class ActivityIndicator extends Vue {
+  data() {
+    return {
+      chosenSize: 30 || this.$props.size
+    }
+  }
+  @Prop(Number) size!: number | string
+}
+</script>
+
+<style lang="scss">
+.activity-indicator {
+  max-height: 500px;
+  max-width: 500px;
+
+  animation: 1s spinning infinite;
+   animation-timing-function: linear;
+}
+@keyframes spinning {
+  0% {
+    transform: rotateZ(0deg)
+  }
+  100% {
+    transform: rotateZ(360deg)
+  }
+}
+</style>
