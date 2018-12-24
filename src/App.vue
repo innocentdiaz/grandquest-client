fb95557991ce7cb06e0d6b302e6032f564610d9c
John S <diaz.john312@gmail.com>
2018-12-23 19:46:04 -0500

    move styling location

--- src/App.vue
+++ src/App.vue
@@ -26,7 +26,7 @@ export default class App extends Vue {
 
     if (JWT) {
       this.fetchUser(JWT);
-    } else if (cachedUser) {
+    } else if (cachedUser /* make sure the user is not online */) {
       this.setUser(JSON.parse(cachedUser));
     } else {
       this.setUserUnauthorized();
@@ -39,6 +39,7 @@ export default class App extends Vue {
 $mainBlue: #036ca5;
 $mainBlack: rgb(24, 24, 24);
 $mainGrey: rgb(179, 179, 179);
+$mainBlueHover: #005e91;
 
 body {
   padding: 0;
@@ -47,9 +48,18 @@ body {
   font-family: 'Roboto', sans-serif;
 }
 
+button {
+  cursor: pointer;
+}
+
 .root {
   height: 100vh;
   width: 100%;
+}
+
+a {
+  color: $mainBlue;
+  font-decoration: none;
 }
 
 .admin-label {
@@ -58,8 +68,65 @@ body {
   border-radius: 5px;
   padding: 5px;
 }
-  a{
+.subtitle {
+  color: $mainGrey;
+  font-weight: lighter;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+  align-items: left;
+  text-align: left;
+  label {
     color: $mainBlue;
-    font-decoration: none;
+    font-size: medium;
   }
+
+  input {
+    border: 0px;
+    padding: .5rem .25rem .5rem .1rem;
+    margin-bottom: 1.8em;
+    font-size: 1rem;
+
+    transition: .2s all ease-in-out;
+    border-bottom: 2px solid $mainBlue;
+    &:disabled {
+      background: rgb(216, 216, 216);
+    }
+    &:invalid {
+      border-bottom: 2px solid red;
+      box-shadow: none;
+    }
+  }
+  select {
+    border: 0;
+    background: rgb(228, 228, 228);
+    padding: .5rem;
+    border-radius: 5px;
+  }
+  button {
+    background: $mainBlue;
+    color: white;
+    font-size: large;
+    border-radius: 5px;
+    border: 0;
+    margin-top: 1em;
+    padding: .5rem;
+
+    transition: .1s all ease-in-out;
+
+    &:hover {
+      cursor: pointer;
+      background: $mainBlueHover;
+    }
+    &:disabled {
+      cursor: default !important;
+      background: $mainGrey;
+    }
+  }
+  .error {
+    color: rgb(255, 49, 49);
+  }
+}
 </style>
