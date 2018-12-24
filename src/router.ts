6517eb6c56dafea33bbcfdd8c48f79b8c1c45283
John S <diaz.john312@gmail.com>
2018-12-24 16:07:25 -0500

    added post submitting form

--- src/router.ts
+++ src/router.ts
@@ -1,11 +1,19 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+
 import Home from './views/Home.vue';
 import About from './views/About.vue';
 import DevLog from './views/DevLog.vue';
+
+// forum
 import Forum from './views/forum/Forum.vue';
 import Board from './views/forum/Board.vue';
 import Post from './views/forum/Post.vue';
+
+// submit
+import submitPost from './views/submit/Post.vue';
+
+// auth
 import LogIn from './views/LogIn.vue';
 
 Vue.use(Router);
@@ -34,14 +42,19 @@ export default new Router({
       component: Forum,
     },
     {
-      path: '/forum/:forum/:board_id',
+      path: '/board/:board_id',
       name: 'board',
       component: Board,
     },
     {
-      path: '/forum/:forum/:board_id/:post_id',
+      path: '/post/:post_id',
       name: 'post',
       component: Post,
+    },
+    {
+      path: '/submit/post/:board_id',
+      name: 'submitPost',
+      component: submitPost,
     },
     {
       path: '/login',
