5e7ce74898242f8da634352c577612cac91e2594
John S <diaz.john312@gmail.com>
2019-02-23 23:43:30 -0500

    add basic exploration map using vue and phaser

---
+++ src/game/places/map.ts
@@ -0,0 +1,239 @@
+import Phaser from 'phaser';
+import _ from 'underscore';
+
+/*
+  Assets
+*/
+import MonokaiBgImage from '@/assets/img/backgrounds/monokai-village.png';
+import PotionsShopLabelImage from '@/assets/img/icon/monokai-village/potions-shop.png';
+import store from '@/store';
+
+const newGame = (global: GameInterface): any => {
+  const game = new Phaser.Game({
+    type: Phaser.AUTO,
+    pixelArt: true,
+    // width set to 98% of window width withing range of 300 and 920 pixels
+    width: window.innerWidth,
+    height: window.innerHeight,
+    // backgroundColor: '#7fb8f9',
+    parent: 'canvas-parent',
+    physics: {
+      default: 'arcade',
+      arcade: {
+        gravity: { y: 0 },
+      },
+    },
+    scene: {
+      preload() {
+        let canvasParent = document.getElementById('canvas-parent');
+        if (!canvasParent) {
+          throw new Error('Phaser.js expected a parent element for the canvas, but at time of creating got none');
+        }
+      },
+      create() {
+        const self: any = this;
+        /*
+          Bind gameInterface.actions to `self`
+        */
+        _.each(actions, (func: any, action: string) => {
+          actions[action] = func.bind(self);
+        });
+        /*
+          Bind minor helper functions
+        */
+        self.vw = (str: string) => {
+          const num = parseInt(str.substring(0, str.length - 1));
+          const decimal = num / 100;
+          return global._bg.displayWidth * decimal;
+        }
+        self.vh = (str: string) => {
+          const num = parseInt(str.substring(0, str.length - 1));
+          const decimal = num / 100;
+          return global._bg.displayHeight * decimal;
+        }
+
+        let assetsLoaded = 0;
+        _.each([
+          { name: 'monokai-bg', type: 'image', src: MonokaiBgImage, spriteDimensions: [] },
+          { name: 'potions-shop-label', type: 'image', src: PotionsShopLabelImage, spriteDimensions: [] },
+        ], (a, i, l) => {
+          const { name, src, type, spriteDimensions } = a;
+
+          const img = new Image();
+          img.src = src;
+
+          // method to add texture `onload` according to type
+          const method = type === 'image'
+          ? () => {
+              self.textures.addImage(name, img);
+            }
+          : type === 'spritesheet' && spriteDimensions
+          ? () => {
+              self.textures.addSpriteSheet(
+                name,
+                img,
+                { frameWidth: spriteDimensions[0], frameHeight: spriteDimensions[1] }
+              );
+            }
+          : () => {};
+
+          // image onload event
+          img.onload = () => {
+            method();
+            assetsLoaded++;
+            if (assetsLoaded === l.length) {
+              actions.loadScene();
+              global.gameInitialized = true;
+              self.cameras.main.fadeIn(750);
+            }
+          }
+        });
+      },
+      update() {
+        const self: any = this;
+        if (!global.gameInitialized) {
+          return
+        }
+        
+        // move camera
+        if (self.input.x < self.game.canvas.offsetWidth * 0.25) {
+          self.cameras.main.scrollX -= 5.5;
+        }
+        if (self.input.x > self.game.canvas.offsetWidth * 0.75) {
+          self.cameras.main.scrollX += 5.5;
+        }
+        if (self.input.y < self.game.canvas.offsetHeight * 0.25) {
+          self.cameras.main.scrollY -= 5.5;
+        }
+        if (self.input.y > self.game.canvas.offsetHeight * 0.75) {
+          self.cameras.main.scrollY += 5.5;
+        }
+
+        // update sky colors
+        global._skyGradient.clear();
+        const worldHour = new Date(store.state.world.timeOfDay).getHours();
+        if (worldHour >= 5 && worldHour < 9) {
+          // morning
+          global._skyGradient.fillGradientStyle(0x89b8ff, 0x89b8ff, 0xfdaea3, 0xfdaea3);
+        } else if (worldHour >= 9 && worldHour < 19) {
+          // day
+          global._skyGradient.fillGradientStyle(0x1c6fcf, 0x1c6fcf, 0x95d6f8, 0x95d6f8);
+        } else if (worldHour >= 19 && worldHour < 21) {
+          // evening
+          global._skyGradient.fillGradientStyle(0x52adff, 0x52adff, 0xed8d45, 0xed8d45);
+        } else {
+          // night
+          global._skyGradient.fillGradientStyle(0x01071f, 0x01071f, 0x663c94, 0x663c94);
+        }
+        global._skyGradient
+          .fillRect(0, 0, global._bg.displayWidth, global._bg.displayHeight)
+          .setDepth(1);
+      }
+    }
+  });
+
+  /*
+    Game actions
+    (All methods within are binded to the game instance on game creation)
+  */
+  let actions: { [action: string]: any } = {
+    loadScene() {
+      const self: any = this;
+
+      /*
+        z-index 1 = sky gradient
+        z-index 5 = map
+        z-index 8 = labels
+      */
+      
+      // map background
+      global._bg = self.add.image(0, 0, 'monokai-bg')
+        .setScale(self.game.canvas.offsetWidth / 980)
+        .setOrigin(0, 0)
+        .setDepth(5);
+        
+      // sky gradient
+      global._skyGradient = self.add.graphics()
+          .fillGradientStyle(0x89b8ff, 0x89b8ff, 0xfdaea3, 0xfdaea3, 1)
+          .fillRect(0, 0, global._bg.displayWidth, global._bg.displayHeight)
+          .setDepth(1);
+
+      // configure main camera
+      self.cameras.main
+        .setZoom(1.25)
+        .setBounds(0, 0, global._bg.displayWidth, global._bg.displayHeight - (global._bg.displayHeight * 0.10));
+
+      // configure labels
+      let potionsShopLabel = self.add.image(self.vw('60%'), self.vh('28%'), 'potions-shop-label')
+        .setAlpha(0.75)
+        .setDisplaySize(self.vw('20%'), self.vw('20%')*(99/502))
+        .setDepth(8)
+        .setInteractive()
+        .on('pointerover', () => {
+          potionsShopLabel.setAlpha(1);
+          global.tooltip = {
+            title: 'Potions Shop',
+            description: 'Lorem Ipsum',
+          }
+        })
+        .on('pointerout', () => {
+          potionsShopLabel.setAlpha(0.75);
+          global.tooltip = {};
+        })
+        .on('pointerup', (pointer: any) => {
+          // if pointer is still within image bounds when click is released
+          if (!pointer.wasCancelled) {
+            console.log('enter potions shop');
+          }
+        });
+    },
+  }
+
+  return game;
+}
+
+/**
+* Contains an interface for sharing data outside of the Phaser.Game instance
+*/
+interface GameInterface {
+  gameInitialized: boolean;
+  tooltip: {
+    title?: string;
+    description?: string;
+  };
+  _bg: any;
+  _skyGradient: any;
+  launch: () => void;
+  destroyGame: () => void;
+  resizeMonitor: () => void;
+}
+
+/**
+* This is the game interface creator
+* @returns returns a new GameInterface
+*/
+export default (): GameInterface => {
+  let game: any = null;
+
+  let global: GameInterface = {
+    tooltip: {},
+    _bg: null,
+    _skyGradient: null,
+    gameInitialized: false,
+    launch() {
+      if (!game) {
+        game = newGame(global);
+      }
+    },
+    destroyGame() {
+      if (game) {
+        game.destroy();
+      }
+    },
+    resizeMonitor() {
+      game.resize(window.innerWidth, window.innerHeight);
+    },
+  };
+
+  return global;
+};
---
+++ src/views/Map.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="map">
+    <button class="exit-button" v-on:click="() => $router.replace({ name: 'world' })">
+      EXIT
+    </button>
+    <div class="tool-tip" v-if="gameInterface.tooltip.title">
+      <h2>{{gameInterface.tooltip.title}}</h2>
+      <p>{{gameInterface.tooltip.description}}</p>
+    </div>
+    <div id="canvas-parent">
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import { State, Mutation } from 'vuex-class'
+import gameInterface from '@/game/places/map.ts';
+import { Player, SocketState } from '@/types';
+
+@Component
+export default class Map extends Vue {
+  @State public player!: Player;
+  @State public socket!: SocketState;
+  @Mutation public SET_HEADER_VISIBILITY: any;
+
+  public gameInterface = gameInterface();
+  public mounted() {
+    if (!this.socket.connected || !this.player.authenticated) {
+      return this.$router.replace({ name: 'world' });
+    }
+    this.SET_HEADER_VISIBILITY(false);
+    this.gameInterface.launch();
+    window.addEventListener('resize', this.gameInterface.resizeMonitor, true);
+  }
+  public destroyed() {
+    this.SET_HEADER_VISIBILITY(true);
+    window.removeEventListener('resize', this.gameInterface.resizeMonitor, true);
+    this.gameInterface.destroyGame();
+  }
+}
+</script>
+<style lang="scss" scoped>
+.map {
+  background: black;
+  height: 100vh;
+  width: 100vw;
+  overflow: hidden;
+  .exit-button {
+    position: absolute;
+    top: 10px;
+    left: 10px;
+    font-family: 'Lora', serif;
+    background: #d30938;
+    color: white;
+    font-weight: bold;
+    border: none;
+    padding: 1em 2em;
+    transition: .2s all ease-in-out;
+  }
+  .tool-tip {
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    background: rgba(24, 24, 24, 0.856);
+    min-height: 50px;
+    font-family: 'Press Start 2P', monospace;
+    padding: 25px 10px;
+    color: white;
+    h2 {
+      margin: 0;
+    }
+  }
+}
+</style>
