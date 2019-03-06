c99fcdd8f6e9852ce8f7e7fc2bd1de1db2e19042
John S <diaz.john312@gmail.com>
2019-03-05 19:56:10 -0500

    update log in view style

--- src/views/LogIn.vue
+++ src/views/LogIn.vue
@@ -1,32 +1,27 @@
 <template>
   <div class="log-in" v-if="!player.authenticated">
-    <aside>
-      <h1>GrandQuest: live feed!</h1>
-      [ Feed coming soon ]
-    </aside>
-    <div class="content">
-      <h1>Welcome back!</h1>
-      <h2 class="subtitle">Long time no see</h2>
-      <div v-if="formLoading">
-        <ActivityIndicator />
-      </div>
-      <form v-on:submit.prevent="onSubmit" v-else>
-        <p class="form-error" v-if="apiError">
-          {{ apiError }}
-        </p>
-        <label>Email</label>
-        <input type="email" placeholder="GrandQuest email" v-model="email">
-        <div class="input-sub"></div>
-
-        <label>Password</label>
-        <input type="password" placeholder="GrandQuest password" v-model="password">
-        <div class="input-sub"></div>
-
-        <button :disabled="formLoading">
-          Log In
-        </button>
-      </form>
+    <router-link to="/">
+      <img src="@/assets/img/icon/grandquest.png" id="icon" alt="GrandQuest">
+    </router-link>
+    <div v-if="formLoading">
+      <ActivityIndicator />
     </div>
+    <form v-on:submit.prevent="onSubmit" v-else>
+      <p class="form-error" v-if="apiError">
+        {{ apiError }}
+      </p>
+      <label>Email</label>
+      <input type="email" placeholder="GrandQuest email" v-model="email">
+      <div class="input-sub"></div>
+
+      <label>Password</label>
+      <input type="password" placeholder="GrandQuest password" v-model="password">
+      <div class="input-sub"></div>
+
+      <button :disabled="formLoading">
+        Log In
+      </button>
+    </form>
   </div>
   <div class="log-in" v-else>
     <div class="content">
@@ -50,6 +45,7 @@ import { Player } from '@/types';
 })
 export default class LogIn extends Vue {
   @State public player!: Player;
+  @Mutation public SET_HEADER_VISIBILITY: any;
   @Mutation public UPDATE_SOCKET_PLAYER: any;
   @Action public INIT_SOCKET: any;
 
@@ -61,6 +57,10 @@ export default class LogIn extends Vue {
 
   public mounted() {
     this.apiError = null;
+    this.SET_HEADER_VISIBILITY(false);
+  }
+  public destroyed() {
+    this.SET_HEADER_VISIBILITY(true);
   }
   public onSubmit() {
     const { email, password } = this;
@@ -96,13 +96,32 @@ export default class LogIn extends Vue {
 </script>
 <style lang="scss">
 .log-in {
-  padding: 1em;
-
-  aside {
-    margin-right: 3em;
-    float: left;
-    clear: right;
-    min-height: 50vh;
+  height: 100vh;
+  width: 100%;
+  background-image: linear-gradient(to bottom, rgba(116, 0, 154, 0.4) 10%,rgba(76, 116, 138, 0) 40%), linear-gradient(to bottom, rgba(0, 31, 106, 0.85) 0%,rgba(0, 102, 255, 0.4) 100%), linear-gradient(135deg, #ffffff 0%,#00d0ff 100%);
+  text-align: center;
+  #icon {
+    height: 4em;
+    margin: 5em auto 3em auto;
+  }
+  form {
+    padding: 1em;
+    margin: auto auto;
+    width: 75%;
+    border-radius: 6px;
+    box-shadow: 0px 0px 10px #00000085;
+    background-image: linear-gradient(to bottom, rgb(236, 244, 244) 50%, rgb(195, 218, 236) 100%);;
+    input {
+      padding: 6px 8px;
+      color: white;
+      background: #265FA7;
+      border: 1px solid #000527;
+      border-radius: 5px;
+    }
+    button {
+      margin: auto;
+      width: 40%;
+    }
   }
 }
 </style>
